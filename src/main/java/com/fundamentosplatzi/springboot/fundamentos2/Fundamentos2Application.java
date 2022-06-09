53a503f2a9bcb64f037e3ba9868792e1a345885f
pinillax3 <felipe_15-1998@hotmail.com>
2022-06-09 09:01:00 -0500

    first commit

---
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos2/Fundamentos2Application.java
@@ -0,0 +1,13 @@
+package com.fundamentosplatzi.springboot.fundamentos2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fundamentos2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fundamentos2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/fundamentosplatzi/springboot/fundamentos2/Fundamentos2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentosplatzi.springboot.fundamentos2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fundamentos2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
